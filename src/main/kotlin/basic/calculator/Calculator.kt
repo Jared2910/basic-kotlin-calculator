09b3bf4009e9cca9f696a2bd6e9a8703ff2191a6
Jared2910 <jared291095@gmail.com>
2023-03-02 23:29:44 -0600

    Creacion de la clase Calculator

---
+++ src/main/kotlin/basic/calculator/Calculator.kt
@@ -0,0 +1,13 @@
+package design.patterns.basic.calculator
+
+internal class Calculator() {
+    fun suma(numberA: Int, numberB: Int) = numberA + numberB
+
+    fun resta(numberA: Int, numberB: Int) = numberA - numberB
+
+    fun multiplicacion(numberA: Int, numberB: Int) = numberA * numberB
+
+    fun division(numberA: Int, numberB: Int) = numberA / numberB
+
+
+}
